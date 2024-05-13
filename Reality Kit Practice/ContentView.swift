1f6a2fb5e5e1c1538d72fcbb1193457b2766debf
Ayush Singhal <ayushsinghals02@gmail.com>
2024-05-13 23:38:06 +0530

    Backup

--- Reality Kit Practice/ContentView.swift
+++ Reality Kit Practice/ContentView.swift
@@ -5,20 +5,82 @@
 //  Created by Ayush Singhal on 13/05/24.
 //
 
+import Combine
+import RealityKit
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        MyUIView()
+            .ignoresSafeArea()
+    }
+}
+
+struct MyUIView: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> some UIViewController {
+        return GameViewController()
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+}
+
+class GameViewController: UIViewController {
+    var sceneEventsUpdateSubscription: Cancellable!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let arView = ARView(frame: view.frame,
+                            cameraMode: .nonAR,
+                            automaticallyConfigureSession: false)
+        view.addSubview(arView)
+
+        let skyboxName = "aerodynamics_workshop_4k"
+        let skyboxResource = try! EnvironmentResource.load(named: skyboxName)
+        arView.environment.lighting.resource = skyboxResource
+        arView.environment.background = .skybox(skyboxResource)
+
+        var sphereMaterial = SimpleMaterial()
+        sphereMaterial.metallic = MaterialScalarParameter(floatLiteral: 1)
+        sphereMaterial.roughness = MaterialScalarParameter(floatLiteral: 0)
+
+        let sphereEntity = ModelEntity(mesh: .generateSphere(radius: 1), materials: [sphereMaterial])
+        let sphereAnchor = AnchorEntity(world: .zero)
+        sphereAnchor.addChild(sphereEntity)
+        arView.scene.anchors.append(sphereAnchor)
+
+        let camera = PerspectiveCamera()
+        camera.camera.fieldOfViewInDegrees = 60
+
+        let cameraAnchor = AnchorEntity(world: .zero)
+        cameraAnchor.addChild(camera)
+
+        arView.scene.addAnchor(cameraAnchor)
+
+        let cameraDistance: Float = 5
+        var currentCameraRotation: Float = 0
+        let cameraRotationSpeed: Float = 0.01
+
+        sceneEventsUpdateSubscription = arView.scene.subscribe(to: SceneEvents.Update.self) { _ in
+            let x = sin(currentCameraRotation) * cameraDistance
+            let z = cos(currentCameraRotation) * cameraDistance
+
+            let cameraTranslation = SIMD3<Float>(x, 0, z)
+            let cameraTransform = Transform(scale: .one,
+                                            rotation: simd_quatf(),
+                                            translation: cameraTranslation)
+
+            camera.transform = cameraTransform
+            camera.look(at: .zero, from: cameraTranslation, relativeTo: nil)
+
+            currentCameraRotation += cameraRotationSpeed
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+//    GameViewController()
 }
