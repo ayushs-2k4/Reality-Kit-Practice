d3c8d74d1aba3d5b9e338b3cb77428adfcf9131c
Ayush Singhal <ayushsinghals02@gmail.com>
2024-05-13 22:47:58 +0530

    Initial Commit

---
+++ Reality Kit Practice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Reality Kit Practice
+//
+//  Created by Ayush Singhal on 13/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Reality Kit Practice/Reality_Kit_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reality_Kit_PracticeApp.swift
+//  Reality Kit Practice
+//
+//  Created by Ayush Singhal on 13/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Reality_Kit_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
